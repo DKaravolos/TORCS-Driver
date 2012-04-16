6cdc42d31e3574c4520528396de0c762d422e145
Daniel <daniro@gmail.com>
2012-04-16 16:31:10 +0200

    Added RL code, because I am changing that too.

---
+++ rlcpp/CartPoleInterface.cpp
@@ -0,0 +1,34 @@
+# include "CartPoleInterface.h"
+# include "CartPole.h"
+
+CartPoleInterface::CartPoleInterface( const char * pFile ) {
+
+    parameterFile = pFile ;
+
+    parameterPath.assign( parameterFile ) ;
+
+    readParameterFile() ;
+
+}
+/*
+int main( int argn, char *argv[] ) {
+
+    if ( argn != 2 ) {
+
+        cout << "You need to specify exactly one configuration (parameter) file." << endl ;
+        return 0 ;
+
+    }
+
+    CartPoleInterface * interface = new CartPoleInterface( argv[1] ) ;
+
+    CartPole * cartpole = new CartPole() ;
+
+    interface->getParametersAndRunExperiments( cartpole ) ;
+
+    delete cartpole ;
+    delete interface ;
+}
+*/
+
+
