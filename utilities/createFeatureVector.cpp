e4f22fae43e20ed15830ddb4c7eb3e9bf6a81cab
Daniel <daniro@gmail.com>
2012-05-10 13:57:21 +0200

    Changed features of Cacla

--- utilities/createFeatureVector.cpp
+++ utilities/createFeatureVector.cpp
@@ -6,8 +6,14 @@ vector<double> createFeatureVector(CarState state)
 {
 	vector<double> featureVector;
 	featureVector.push_back(state.getAngle()/PI); // Genormaliseerd. Range is tussen -pi en pi.
-	featureVector.push_back(state.getSpeedX()/300);//Genormaliseerd. 300kmh lijkt max. praktisch bijna niet boven 290.
-	featureVector.push_back(state.getSpeedY()); // Geen idee wat we hiermee kunnen qua normalisatie
+	//featureVector.push_back(state.getSpeedX()/300);//Genormaliseerd. 300kmh lijkt max. praktisch bijna niet boven 290.
+	bool speed_x = state.getSpeedX() > 70;
+	featureVector.push_back(double(speed_x));
+	
+	//featureVector.push_back(state.getSpeedY()); // Geen idee wat we hiermee kunnen qua normalisatie
+	bool speed_y = state.getSpeedY() > 0.5;
+	featureVector.push_back(double(speed_y));
+
 	featureVector.push_back(state.getTrackPos()); //Zit al tussen -1 (rechts) en 1 (links). Waarde buiten deze range betekent off-track. 
 	for(int i = 0; i <= 8; i++){
 		featureVector.push_back(state.getTrack(5+i)/200); //200m is de max_range
