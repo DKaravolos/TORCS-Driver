2669a1793f9a868457269e96cb7aefd7bd539e3c
Daniel <daniro@gmail.com>
2012-07-30 11:13:15 +0200

    CaclaDriver ook maar eens toegevoegd aan de repository.

---
+++ driver/CaclaDriver.h
@@ -0,0 +1,167 @@
+#ifndef CaclaDriver_H_
+#define CaclaDriver_H_
+
+#include <conio.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include "BaseDriver.h"
+#include "CarState.h"
+#include "CarControl.h"
+#include "SimpleParser.h"
+#include "WrapperBaseDriver.h"
+//
+#include <vector>
+//Functions/classes by Daniel:
+#include "..\utilities\createFeatureVector.h"
+#include "..\utilities\printFeatureVector.h"
+#include "..\learning\CaclaLearningI.h"
+#include "..\utilities\Writer.h"
+
+
+#define PI 3.14159265
+
+using namespace std;
+
+class CaclaDriver : public WrapperBaseDriver
+{
+public:
+	
+	// Constructor
+	CaclaDriver();
+
+	// SimpleDriver implements a simple and heuristic controller for driving
+	virtual CarControl wDrive(CarState cs);
+
+	// Print a shutdown message 
+	virtual void onShutdown();
+	
+	// Print a restart message 
+	virtual void onRestart();
+
+	// Initialization of the desired angles for the rangefinders
+	virtual void init(float *angles);
+
+private:
+	
+	/* Gear Changing Constants*/
+	
+	// RPM values to change gear 
+	static const int gearUp[6];
+	static const int gearDown[6];
+		
+	/* Stuck constants*/
+	
+	// How many time steps the controller wait before recovering from a stuck position
+	static const int stuckTime;
+	// When car angle w.r.t. track axis is grather tan stuckAngle, the car is probably stuck
+	static const float stuckAngle;
+	
+	/* Steering constants*/
+	
+	// Angle associated to a full steer command
+	static const float steerLock;	
+	// Min speed to reduce steering command 
+	static const float steerSensitivityOffset;
+	// Coefficient to reduce steering command at high speed (to avoid loosing the control)
+	static const float wheelSensitivityCoeff;
+	
+	/* Accel and Brake Constants*/
+	
+	// max speed allowed
+	static const float maxSpeed;
+	// Min distance from track border to drive at  max speed
+	static const float maxSpeedDist;
+	// pre-computed sin5
+	static const float sin5;
+	// pre-computed cos5
+	static const float cos5;
+	
+	/* ABS Filter Constants */
+	
+	// Radius of the 4 wheels of the car
+	static const float wheelRadius[4];
+	// min slip to prevent ABS
+	static const float absSlip;						
+	// range to normalize the ABS effect on the brake
+	static const float absRange;
+	// min speed to activate ABS
+	static const float absMinSpeed;
+
+	/* Clutch constants */
+	static const float clutchMax;
+	static const float clutchDelta;
+	static const float clutchRange;
+	static const float clutchDeltaTime;
+	static const float clutchDeltaRaced;
+	static const float clutchDec;
+	static const float clutchMaxModifier;
+	static const float clutchMaxTime;
+
+	// counter of stuck steps
+	int stuck;
+	
+	// current clutch
+	float clutch;
+
+	// Solves the gear changing subproblems
+	int getGear(CarState &cs);
+
+	// Solves the steering subproblems
+	float getSteer(CarState &cs);
+	
+	// Solves the gear changing subproblems
+	float getAccel(CarState &cs);
+	
+	// Apply an ABS filter to brake command
+	float filterABS(CarState &cs,float brake);
+
+	// Solves the clucthing subproblems
+	void clutching(CarState &cs, float &clutch);
+
+	////////Functions added by Daniel:
+	void initInterface(bool load_network);
+	double computeReward(CarState &state, double* action, CarState &next_state);
+	void doLearning(CarState &cs);
+	CarControl carStuckControl(CarState &cs);
+	CarControl simpleBotControl(CarState &cs);
+	CarControl rlControl(CarState &cs);
+	void endOfRunCheck(CarState &cs, CarControl &cc);
+	char getKeyboardInput();
+
+	///////Datamembers added by Daniel:
+	vector<double>* mp_features;
+	CaclaLearningI* mp_Qinterface;
+	double* mp_action_set;
+	Writer* mp_log;
+	Writer* mp_reward_writer;
+
+	
+	//Elements for computing rewards
+	CarState* gp_prev_state;
+	
+	//time counters
+	int g_count;
+	int g_learn_step_count;
+	int g_stuck_step_count;
+	int g_reupdate_step_count;
+	int g_experiment_count;
+
+	//flags for learning
+	bool g_learning_done;
+	bool g_first_time;
+
+	//learning parameters
+	int g_stuck_penalty;
+	int g_steps_per_action;
+	int g_learn_steps_per_tick;
+	int g_reupdate_steps_per_tick;
+
+	//debug parameters
+	int g_print_mod;
+	int debug_stuck_count;
+	int debug_rlcontrol_count;
+};
+
+#endif /*CaclaDriver_H_*/
